bfc369df07a826220329d834f00eb7c03522a57d
Aidan McDonald <aidan@mcdcentral.org>
2017-12-05 11:12:02 -0800

    Better initial guess and general update

--- TestCIETstep.m
+++ TestCIETstep.m
@@ -1,11 +1,17 @@
-a.T = [800;300;300;300;273;300;300;300];
+clear all;
+close all;
 
-for i = 1:10000
-a.dt = 0.1;
+tstart = tic;
+numSteps = 1000000;
+a.T = [200;102;101;80;20;80;80;79] + 273;
+%a.T = [290;290;290;290;290;290;290;290];
+
+for i = 1:numSteps
+a.dt = .01;
 a.mdot = 0.18;
-a.P_in = 1000;
-a.P_reject = 1000;
-a.Qdot_pump = 10;
+a.P_in = 8000;
+a.P_reject = 7900;
+a.Qdot_pump = 100;
 
 newT = CIETstep(a);
 
@@ -20,3 +26,7 @@ T_8(i) = newT(8);
 a.T = newT;
 
 end
+telapsed = toc(tstart);
+
+t = 1:numSteps;
+plot(t,T_1,t,T_2,t,T_3,t,T_4,t,T_5,t,T_6,t,T_7,t,T_8)
