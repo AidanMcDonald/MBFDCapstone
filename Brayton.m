d7c3ebfb2971ba39346c912bd8660fe91ab94d71
Aidan McDonald <aidan@mcdcentral.org>
2018-03-08 16:36:17 -0800

    Update to use polytropic efficiencies

--- Brayton.m
+++ Brayton.m
@@ -4,15 +4,30 @@ function output = Brayton(input)
 % Authors: Aidan McDonald
 
 %% Get Input Values/Assumed Values
+
 % Compression ratio
 if isfield(input,'beta')
     beta = input.beta;
 else
-    beta = 20;
+    beta = 19.6 ;
 end
 
-% Gamma of air - intrinsic property of air
-gamma = (1.4-1)/1.4;
+% Expansion ratio 1
+if isfield(input,'beta_esp1')
+    beta_esp1 = input.beta_esp1;
+else
+    beta_esp1 = 3.77 ;
+end
+
+% Expansion ratio 2
+if isfield(input,'beta_esp2')
+    beta_esp2 = input.beta_esp2;
+else
+    beta_esp2 = 5.13;
+end
+
+% Don't know the name of this one
+gamma = (1.38-1)/1.38; 
 
 % Atmospheric incoming air
 if isfield(input,'P_1')
@@ -23,71 +38,50 @@ end
 if isfield(input,'T_1')
     T_1 = input.T_1;
 else
-    T_1 = 293; % [K]
+    T_1 = 298; % [K]
 end
 
-% HPT inlet temp
-if isfield(input,'T_3')
-    T_3 = input.T_3;
-else
-    T_3 = 1000; % [K]
-end
-
-% LPT inlet temp
-if isfield(input,'T_5')
-    T_5 = input.T_5;
-else
-    T_5 = 1000; % [K]
-end
-
-% Efficiencies
+% Polytropic efficiencies
 if isfield(input,'eta_C')
     eta_C = input.eta_C;
 else
-    eta_C = .95; % compressor
+    eta_C = .70; % compressor
 end
 if isfield(input,'eta_T1')
     eta_T1 = input.eta_T1;
 else
-    eta_T1 = .95; % high pressure turbine
+    eta_T1 = .80; % high pressure turbine
 end
 if isfield(input,'eta_T2')
     eta_T2 = input.eta_T2;
 else
-    eta_T2 = .95; % low pressure turbine
+    eta_T2 = .80; % low pressure turbine
 end
+
 
 % Work from HPT
 if isfield(input,'Wdot_T1')
     Wdot_T1 = input.Wdot_T1;
 else
-    Wdot_T1 = 100E6; % [W]
+    Wdot_T1 = 1e6; % [W]
 end
 
 % Work from LPT
 if isfield(input,'Wdot_T2')
     Wdot_T2 = input.Wdot_T2;
 else
-    Wdot_T2 = 100E6; % [W]
+    Wdot_T2 = 137.94E6; % [W]
 end
 
-% Pressure drops in heat exchangers
-if isfield(input,'deltaP_1')
-    deltaP_1 = input.deltaP_1;
-else
-    deltaP_1 = 0;
-end
-if isfield(input,'deltaP_2')
-    deltaP_2 = input.deltaP_2;
-else
-    deltaP_2 = 0;
-end
+% Pressure drops in heat exchanger
+deltaP_1 = 0;
+deltaP_2 = 0;
 
 % Mass flowrate
 if isfield(input,'mdot')
     mdot = input.mdot;
 else
-    mdot = 150; % [kg/s]
+    mdot = 418; % [kg/s]
 end
 
 % Specific heat
@@ -102,21 +96,24 @@ P_2s = beta*P_1;
 T_2s = beta^gamma*T_1;
 
 P_2 = P_2s;
-T_2 = (T_2s-T_1)/eta_C+T_1;
+T_2 = T_1 * beta^(gamma/eta_C);
 
 P_3 = P_2 - deltaP_1;
+T_3 = (Wdot_T1) / (mdot * c_p * (1 - beta_esp1^(- gamma * eta_T1)) )
 
-T_4 = T_3 - Wdot_T1/(mdot*c_p);
-T_4s = T_3 - (T_3-T_4)/eta_T1;
-P_4s = P_3*(T_4s/T_3)^(1/gamma);
-P_4 = P_4s;
+T_4 = T_3 * beta_esp1^(- gamma * eta_T1);
+P_4 = P_3 / beta_esp1;
 
 P_5 = P_4 - deltaP_2;
+T_5 = (Wdot_T2) / (mdot * c_p * (1 - beta_esp2^(- gamma * eta_T2)) )
 
-T_6 = T_5 - Wdot_T2/(mdot*c_p);
-T_6s = T_5 - (T_5-T_6)/eta_T2;
-P_6s = beta*P_5*(T_6s/T_5)^(1/gamma);
-P_6 = P_6s;
+T_6 = T_5 * beta_esp2^(- gamma * eta_T2);
+P_6 = P_5 / beta_esp2;
+
+% Power
+P_th1 = mdot * c_p * (T_3 - T_2);
+P_th2 = mdot * c_p * (T_5 - T_4);
+
 
 %% Assemble Output
 output.P_1 = P_1;
@@ -131,4 +128,6 @@ output.P_5 = P_5;
 output.T_5 = T_5;
 output.P_6 = P_6;
 output.T_6 = T_6;
+output.P_th1 = P_th1;
+output.P_th2 = P_th2;
 end
